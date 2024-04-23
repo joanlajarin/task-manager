50704e517f31332ccf5dda97f2c745660ad86f43
Joan Lajarin <joanlajarinreinares@gmail.com>
2024-04-23 14:50:55 +0200

    Vite (React + tailwind)

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2 className=' text-3xl font-bold text-red-500 underline text-center'>TASK MANAGER</h2>
+    </>
+  )
+}
+
+export default App
